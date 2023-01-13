1befd1dc60ed5421f208d972274f23cf37947aab
TanvirFahimBD <tanvirfahim6939@gmail.com>
2023-01-13 15:35:46 +0600

    all done

--- history.js
+++ history.js
@@ -5,7 +5,7 @@ function addHistory(questionText, timeTaken, errorCount) {
   newRow.classList.add("col");
 
   newRow.innerHTML = `
-      <div class="card bg-dark p-3 border">
+      <div class="card bg-dark p-3 border history-div">
         <h3 class="card-title">${questionText}</h3>
         <div>
         <p class="card-text">You took: <span class="bold">${timeTaken}</span> seconds</p>
@@ -32,7 +32,7 @@ function displayHistory() {
     newRow.classList.add("col");
 
     newRow.innerHTML = `
-    <div class="card bg-dark p-3 border">
+    <div class="card bg-dark p-3 border history-div">
       <h3 class="card-title">${test.questionText}</h3>
       <p class="card-text">You took: <span class="bold">${test.timeTaken}</span> seconds</p>
       <p class="card-text">You made <span class="bold red">${test.errorCount}</span> mistakes</p>
