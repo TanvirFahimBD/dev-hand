19a744accd08d68b5524726f35095b997c76f8a2
TanvirFahimBD <tanvirfahim6939@gmail.com>
2023-01-13 12:21:14 +0600

    typing result showed middle of the page fixed

--- script.js
+++ script.js
@@ -83,10 +83,10 @@ const gameOver = () => {
   display.classList.add("inactive");
   // show result
   resultModal.innerHTML += `
-    <h1>Finished!</h1>
-    <p>You took: <span class="bold">${timeTaken}</span> seconds</p>
-    <p>You made <span class="bold red">${errorCount}</span> mistakes</p>
-    <button onclick="closeModal()">Close</button>
+      <h1>Finished!</h1>
+      <p>You took: <span class="bold">${timeTaken}</span> seconds</p>
+      <p>You made <span class="bold red">${errorCount}</span> mistakes</p>
+      <button onclick="closeModal()">Close</button>
   `;
 
   addHistory(questionText, timeTaken, errorCount);
